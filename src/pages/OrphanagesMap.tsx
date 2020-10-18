4dc62c448a0e6084d1de4e7252d54c65c793b860
Cleberson <Silva>
2020-10-17 22:03:41 -0300

    new pages
    -create pages createOrphanages
    - Orphanages

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -11,6 +12,10 @@ import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/global.css';
 import '../styles/pages/orphanages-map.css';
 
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg
+});
 
 function OrphanagesMap() {
   return (
@@ -32,9 +37,16 @@ function OrphanagesMap() {
         style={{ width: '100%', height: '100%'}}
       >
        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
-       <TileLayer 
+        <TileLayer 
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+        <Marker 
+          position={[-22.9217234,-47.0680352]}
+          icon = { mapIcon }
+
+        
+        />          
+      
       </Map>
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
